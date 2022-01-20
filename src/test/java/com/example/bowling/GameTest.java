0aff0715a125593150994f82cfda4f57fe1bb106
Oscar Stjernfeldt <oscarstjernfeldt@hotmail.com>
2022-01-20 11:49:33 +0100

    Add test: bowlingGameWithOnlySpares

--- src/test/java/com/example/bowling/GameTest.java
+++ src/test/java/com/example/bowling/GameTest.java
@@ -238,4 +238,36 @@ public class GameTest {
 
         assertThat(result).isEqualTo(300);
     }
+
+    @Test
+    @DisplayName("Verify score after only spares")
+    void bowlingGameWithOnlySpares() {
+        Game game = new Game();
+
+        game.roll(1);
+        game.roll(9);
+        game.roll(2);
+        game.roll(8);
+        game.roll(3);
+        game.roll(7);
+        game.roll(4);
+        game.roll(6);
+        game.roll(5);
+        game.roll(5);
+        game.roll(6);
+        game.roll(4);
+        game.roll(7);
+        game.roll(3);
+        game.roll(8);
+        game.roll(2);
+        game.roll(9);
+        game.roll(1);
+        game.roll(0);
+        game.roll(10);
+        game.roll(5);
+
+        var result = game.score();
+
+        assertThat(result).isEqualTo(149);
+    }
 }
