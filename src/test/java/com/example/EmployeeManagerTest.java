c15305fbd06d2ff8d77be9e141c73e11ad085d12
Oscar Stjernfeldt <oscarstjernfeldt@hotmail.com>
2022-01-20 15:16:10 +0100

    Add test: PayEmployees should return employee list

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,5 +1,28 @@
 package com.example;
 
-class EmployeeManagerTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class EmployeeManagerTest {
+    @Test
+    @DisplayName("payEmployees should return all emlpoyees that have been paid")
+    void payEmployeesShouldReturnAllEmployees() {
+
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        BankService bankService = mock(BankService.class);
+        EmployeeManager employeeManager = new EmployeeManager(employeeRepository, bankService);
+
+        when(employeeRepository.findAll()).thenReturn(
+                List.of(new Employee("Omar", 1000), new Employee("Mergueze", 40000)));
+
+        var result = employeeManager.payEmployees();
+
+        assertThat(result).isEqualTo(2);
+    }
 }
